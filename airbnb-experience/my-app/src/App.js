b54f606566c813293bf8fa3a75c4f6416c769d02
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-01-20 20:34:33 +0000

    the airbnb experience

---
+++ airbnb-experience/my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Card from './Components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
