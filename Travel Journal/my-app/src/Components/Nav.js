19771f7fb3045ea14f23629ca49fec12a83ebf1a
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-01-25 22:24:41 +0000

    Travel Journal

---
+++ Travel Journal/my-app/src/Components/Nav.js
@@ -0,0 +1,12 @@
+import Logo from '../Images/world-icon.png';
+
+const Nav = () => {
+  return (
+    <nav className="Nav">
+      <img src={Logo} alt=""></img>
+      <p>my travel journal.</p>
+    </nav>
+  );
+};
+
+export default Nav;
