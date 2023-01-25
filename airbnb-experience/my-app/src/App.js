2878c568b27986dcdfe3677a6f3a592a2cf0036a
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-01-25 22:12:50 +0000

    airbnb experience

--- airbnb-experience/my-app/src/App.js
+++ airbnb-experience/my-app/src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import Card from './Components/Card';
+import Data from './Components/Data';
 
 function App() {
+  const Cards = Data.map((data) => {
+    return <Card key={data.id} data={data} />;
+  });
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <section className="Cards-list">{Cards}</section>
     </div>
   );
 }
