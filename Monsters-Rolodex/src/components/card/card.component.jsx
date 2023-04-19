f4da6b76687dead97d474357b98ae82934e74f3d
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-04-19 00:18:02 +0000

    Roladex files

---
+++ Monsters-Rolodex/src/components/card/card.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import './card.styles.css';
+
+class Card extends Component {
+  render() {
+    const { name, email, id } = this.props.monster;
+    return (
+      <div className="card-container" key={id}>
+        <img
+          src={`https://robohash.org/${id}?set=set2$size=180x180`}
+          alt="monster"
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
