b76b9b2354437f7153d56c4b4f5206df1fd8eef1
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-04-28 23:25:49 +0000

    navigation component

---
+++ topp-clothing/routes/navigation/navigation.components.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <div className="logo-container">
+          <Link className="nav-link" to="/">
+            <CrwnLogo className="logo" />
+          </Link>
+        </div>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
