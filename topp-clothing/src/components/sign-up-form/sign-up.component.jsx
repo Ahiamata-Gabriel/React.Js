8dc6b5f9f655eba063436f18921e60227eb2d0c1
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-06-26 12:08:46 +0000

    change label

--- topp-clothing/src/components/sign-up-form/sign-up.component.jsx
+++ topp-clothing/src/components/sign-up-form/sign-up.component.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
+import './sign-up.styles.scss';
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
@@ -51,8 +52,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
