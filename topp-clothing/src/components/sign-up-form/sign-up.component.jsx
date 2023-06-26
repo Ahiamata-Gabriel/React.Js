301c655faa7b70417736d105d79c63b0dd192638
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-06-26 11:38:36 +0000

    change label

--- topp-clothing/src/components/sign-up-form/sign-up.component.jsx
+++ topp-clothing/src/components/sign-up-form/sign-up.component.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import FormInput from '../form-input/form-input.component';
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
@@ -53,32 +54,35 @@ const SignUpForm = () => {
     <div>
       <h1>Sign up with your email and password</h1>
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input
+        <FormInput
+          label="Display Name"
           type="text"
           required
           onChange={handleChange}
           name="displayName"
           value={displayName}
         />
-        <label>Email</label>
-        <input
+
+        <FormInput
+          label="Email"
           type="email"
           required
           onChange={handleChange}
           name="email"
           value={email}
         />
-        <label>Password</label>
-        <input
+
+        <FormInput
+          label="Password"
           type="password"
           required
           onChange={handleChange}
           name="password"
           value={password}
         />
-        <label>Confirm Password</label>
-        <input
+
+        <FormInput
+          label="Password"
           type="password"
           required
           onChange={handleChange}
