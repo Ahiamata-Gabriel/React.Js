0f2593846e4f7ffe3af226d955d1f1478920f244
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-06-27 15:18:34 +0000

    created auth component

--- topp-clothing/src/App.js
+++ topp-clothing/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.components';
-import SignIn from './routes/sign-in/sign-in.components';
+import Authentication from './routes/authentication/authentication.components';
 
 const Shop = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
