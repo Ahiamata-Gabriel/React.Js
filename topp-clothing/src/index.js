2f169a80501ddadc1bf278d4c3bd8e45bde6e189
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-07-24 22:36:40 +0000

    added cart icon and dropdown

--- topp-clothing/src/index.js
+++ topp-clothing/src/index.js
@@ -5,6 +5,7 @@ import './index.scss';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +14,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
