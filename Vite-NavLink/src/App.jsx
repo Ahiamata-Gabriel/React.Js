4d3bdbc0a7733c968eb2f4df498ff7fe54b3de25
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-09-05 00:07:45 +0000

    NavLink

---
+++ Vite-NavLink/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
