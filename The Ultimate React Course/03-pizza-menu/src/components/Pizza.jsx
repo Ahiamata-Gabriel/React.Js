7079485c4ec1d699509b99974ad8846031274d5e
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-12-28 00:21:46 +0000

    Pizza app

--- The Ultimate React Course/03-pizza-menu/src/components/Pizza.jsx
+++ The Ultimate React Course/03-pizza-menu/src/components/Pizza.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Pizza = ({ Pizza }) => {
-  const pizzaEl = Pizza.map((pizza) => {
-    return pizza;
-  });
+  const pizzaEl = Pizza.map((pizza) => (
+    <div key={pizza.name}>
+      <h1>{pizza.name}</h1>
+    </div>
+  ));
 
-  return <div>{pizzaEl.name}</div>;
+  return <div>{pizzaEl}</div>;
 };
 
 export default Pizza;
