f106bbdebebd319b7ab2122f55e0f51e318e42d9
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-12-30 02:06:18 +0000

    testing Material Icons

--- e-commence/src/Pages/Home.jsx
+++ e-commence/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../components/NavBar/NavBar';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <AcUnitIcon className="icon" />
     </div>
   );
 };
