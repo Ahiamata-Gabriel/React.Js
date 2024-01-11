b0696ebe9f4a44da7b0b4a92d76716312b2a926c
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-01-11 01:21:22 +0000

    update app

--- e-commence/src/App.jsx
+++ e-commence/src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css';
 import Home from './Pages/Home';
+import ProductList from './Pages/ProductList';
+import Product from './Pages/Product';
+import Register from './Pages/Register';
 
 function App() {
-  return <Home />;
+  return <Register />;
 }
 
 export default App;
