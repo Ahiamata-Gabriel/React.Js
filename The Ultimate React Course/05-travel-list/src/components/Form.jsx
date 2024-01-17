6120c84ebacc48d6662b767c07046446fd844886
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-01-17 00:37:10 +0000

    Travel Expenses

--- The Ultimate React Course/05-travel-list/src/components/Form.jsx
+++ The Ultimate React Course/05-travel-list/src/components/Form.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip</h3>
-    </div>
+      <select name="" id="">
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item.." />
+    </form>
   );
 };
 
