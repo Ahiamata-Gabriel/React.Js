d315db07645e3a0cee5cdeaed65499c8ebd617b3
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-01-28 00:52:57 +0000

    travel list

--- The Ultimate React Course/05-travel-list/src/components/Form.jsx
+++ The Ultimate React Course/05-travel-list/src/components/Form.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+  const [desc, setDesc] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!desc) {
+      return;
+    }
+
+    const newItem = { desc, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDesc('');
+    setQuantity(1);
   };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip</h3>
-      <select name="" id="">
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item.." />
+      <input
+        type="text"
+        placeholder="Item.."
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+      />
+      <button>Add</button>
     </form>
   );
 };
