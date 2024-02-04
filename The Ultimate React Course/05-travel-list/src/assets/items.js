a4773c9826a61be567f0d6e16d44cdc130400d45
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-02-04 00:52:33 +0000

    Travel list

---
+++ The Ultimate React Course/05-travel-list/src/assets/items.js
@@ -0,0 +1,5 @@
+export const Items = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
