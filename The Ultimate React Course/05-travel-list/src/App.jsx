f23c8e836111cb01bf5afec7a61d909c1cb63c40
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-02-07 14:35:24 +0000

    App

--- The Ultimate React Course/05-travel-list/src/App.jsx
+++ The Ultimate React Course/05-travel-list/src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css';
+import { useState } from 'react';
 import Logo from './components/Logo';
 import Form from './components/Form';
 import PackingList from './components/PackingList';
 import Stats from './components/Stats';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
+
+  const handleDeleteItem = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
