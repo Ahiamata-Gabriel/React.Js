f639b88e89a48ea633a02cd44cbb385a816f5047
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-02-09 01:41:13 +0000

    item component

--- The Ultimate React Course/05-travel-list/src/components/Item.jsx
+++ The Ultimate React Course/05-travel-list/src/components/Item.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Item = ({ item }) => {
+const Item = ({ item, onDeleteItem, onToggleItem }) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleCheck = () => {
+    setChecked(!checked);
+  };
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => {
+          onToggleItem(item.id);
+        }}
+      />
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
-        {item.quantity} {item.description}
+        {item.quantity} {item.desc}
       </span>
-      <button className="btn">&times;</button>
+      <button className="btn" onClick={() => onDeleteItem(item.id)}>
+        &times;
+      </button>
     </li>
   );
 };
