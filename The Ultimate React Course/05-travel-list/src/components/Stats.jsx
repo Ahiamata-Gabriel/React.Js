5098eb6aa6ddab5257a9fb129eea0e3a2835a9b0
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-02-10 00:48:07 +0000

    Stats

--- The Ultimate React Course/05-travel-list/src/components/Stats.jsx
+++ The Ultimate React Course/05-travel-list/src/components/Stats.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start Adding some items</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const packed = items.filter((item) => item.packed).length;
+  const packedPercent = Math.round((packed / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {packedPercent === 100
+          ? 'You have everything Covered ! '
+          : ` You have ${numItems} items on your list, and you already packed (
+        ${packedPercent}
+        %)`}
+      </em>
     </footer>
   );
 };
