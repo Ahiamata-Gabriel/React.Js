f55f2f5abe87a9d00790ab47a337b4c920184b33
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-02-10 00:47:25 +0000

    Packing List

--- The Ultimate React Course/05-travel-list/src/components/PackingList.jsx
+++ The Ultimate React Course/05-travel-list/src/components/PackingList.jsx
@@ -1,15 +1,53 @@
-import React from 'react';
-import { Items } from '../assets/items';
+import React, { useState } from 'react';
 import Item from './Item';
 
-const PackingList = () => {
+const PackingList = ({ items, onDeleteItem, onToggleItem, onClearItems }) => {
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortedItems;
+
+  if (sortBy === 'input') {
+    sortedItems = items;
+  }
+
+  if (sortBy === 'description') {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.desc.localeCompare(b.description));
+  }
+
+  if (sortBy === 'packed') {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {Items.map((item) => (
-          <Item key={item.id} item={item} />
+        {sortedItems.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
+      <div className="actions">
+        <select
+          name=""
+          id=""
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="input"> sort by input order</option>
+          <option value="description"> sort by description</option>
+          <option value="packed"> sort by packed status</option>
+        </select>
+
+        <button onClick={onClearItems}>Clear</button>
+      </div>
     </div>
   );
 };
