db4711c4ed8d6b374636cf63f505310859e89d81
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-02-12 17:40:48 +0000

    Tip calculator Challenge

---
+++ The Ultimate React Course/Challenge-1__tip-calculator-solution/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import TipCalculator from './components/TipCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <TipCalculator />
+    </div>
+  );
+}
+
+export default App;
