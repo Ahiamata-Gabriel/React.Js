f9ab9a4a6f5d1031e8012c32da6cdef52b25c2f2
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-02-23 21:21:11 +0000

    Button

---
+++ The Ultimate React Course/04-steps/src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = ({ textColor, bgColor, onClick, children }) => {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
