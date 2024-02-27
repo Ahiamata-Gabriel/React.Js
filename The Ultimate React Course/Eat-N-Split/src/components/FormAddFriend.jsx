f559556d3cb9062a3b36275f379135e78ce68a11
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-02-27 09:59:42 +0000

    FormAddFriend.jsx

---
+++ The Ultimate React Course/Eat-N-Split/src/components/FormAddFriend.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Button from './Button';
+
+const FormAddFriend = () => {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('https://i.pravatar.cc/48');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+    setName('');
+    setImage('https://i.pravatar.cc/48');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-add-friend">
+      <label> Friend Name</label>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+      />
+
+      <label> Image URL</label>
+      <input
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+        type="text"
+      />
+
+      <Button>Add</Button>
+    </form>
+  );
+};
+
+export default FormAddFriend;
