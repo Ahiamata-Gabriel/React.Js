e7497572d7db6937fa00637f0c0eca91e8412c85
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-02-28 09:16:22 +0000

    App.jsx

--- The Ultimate React Course/Eat-N-Split/src/App.jsx
+++ The Ultimate React Course/Eat-N-Split/src/App.jsx
@@ -3,19 +3,26 @@ import FormAddFriend from './components/FormAddFriend';
 import FriendsList from './components/FriendsList';
 import Button from './components/Button';
 import FormSplitBill from './components/FormSplitBill';
+import { initialFriends } from './assets/Data';
 import { useState } from 'react';
 
 function App() {
+  const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
 
   const handleShowAddFriend = () =>
     setShowAddFriend((showAddFriend) => !showAddFriend);
 
+  const handleAddFriend = (friend) => {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  };
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendsList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
         <Button onclick={handleShowAddFriend}>
           {showAddFriend ? 'Close' : 'Add friend'}
         </Button>
