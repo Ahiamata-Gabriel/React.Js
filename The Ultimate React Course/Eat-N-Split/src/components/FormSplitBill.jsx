de1b886d7e62ccab8e4a503f3c9eb2450396086a
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-02-29 09:24:06 +0000

    FormSplitBill.jsx

--- The Ultimate React Course/Eat-N-Split/src/components/FormSplitBill.jsx
+++ The Ultimate React Course/Eat-N-Split/src/components/FormSplitBill.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Button from './Button';
 
-const FormSplitBill = () => {
+const FormSplitBill = ({ selectedFriend }) => {
   return (
     <form className="form-split-bill">
-      <h2>Split a bill with X</h2>
+      <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>Bill value</label>
       <input type="text" />
@@ -12,14 +12,14 @@ const FormSplitBill = () => {
       <label>Your expense</label>
       <input type="text" />
 
-      <label>X`s expense</label>
+      <label>{selectedFriend.name}`s expense</label>
       <input type="text" disabled />
 
       <label> Who is paying the bill</label>
 
       <select>
         <option value="user">You</option>
-        <option value="friend">X</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
 
       <Button>Split Bill</Button>
