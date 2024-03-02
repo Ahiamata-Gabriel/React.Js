0fb577dd347267566c2548ea4d89c811de1fc118
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-02 03:29:13 +0000

    ListBox.jsx

---
+++ The Ultimate React Course/07-usePopcorn/src/components/ListBox.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import MovieList from './MovieList';
+
+const ListBox = () => {
+  const [isOpen1, setIsOpen1] = useState(true);
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen1((open) => !open)}
+      >
+        {isOpen1 ? '–' : '+'}
+      </button>
+      {isOpen1 && <MovieList />}
+    </div>
+  );
+};
+
+export default ListBox;
