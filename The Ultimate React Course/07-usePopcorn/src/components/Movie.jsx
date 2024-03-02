b99f06c861caca7b1cafea44ab14bbe8a979bd7d
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-02 03:30:51 +0000

    Movie.jsx

---
+++ The Ultimate React Course/07-usePopcorn/src/components/Movie.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Movie = ({ movie }) => {
+  return (
+    <li>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
+  );
+};
+
+export default Movie;
