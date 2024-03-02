b78594995f455ee1a8ef73ec916ff86d8e6ee8a2
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-02 03:30:10 +0000

    Main.jsx

---
+++ The Ultimate React Course/07-usePopcorn/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import ListBox from './ListBox';
+import WatchedBox from './WatchedBox';
+
+const Main = () => {
+  return (
+    <main className="main">
+      <ListBox />
+      <WatchedBox />
+    </main>
+  );
+};
+
+export default Main;
