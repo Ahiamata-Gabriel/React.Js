308d3a676eea12269bfa86999a219aef45a70987
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-04 10:35:15 +0000

    App.jsx

--- The Ultimate React Course/07-usePopcorn/src/App.jsx
+++ The Ultimate React Course/07-usePopcorn/src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+import { tempMovieData } from './assets/Data';
+import { useState } from 'react';
 
-function App() {
-  return <>Hello</>;
+export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+  return (
+    <>
+      <Navigation movies={movies} />
+      <Main movies={movies} />
+    </>
+  );
 }
-
-export default App;
