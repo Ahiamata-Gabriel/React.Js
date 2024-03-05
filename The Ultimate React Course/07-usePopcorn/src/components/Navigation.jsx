a5538c1a9174139299d27363bbb1341abd14132f
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-05 01:34:56 +0000

    Navigation.jsx

--- The Ultimate React Course/07-usePopcorn/src/components/Navigation.jsx
+++ The Ultimate React Course/07-usePopcorn/src/components/Navigation.jsx
@@ -1,13 +1,9 @@
 import Logo from './Logo';
-import NumResults from './NumResults';
-import Search from './Search';
-
-const Navigation = ({ movies }) => {
+const Navigation = ({ children }) => {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   );
 };
