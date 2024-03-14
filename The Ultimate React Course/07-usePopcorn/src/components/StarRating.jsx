e82d676b9d1168200f89659f8be501b225456d17
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-14 10:35:57 +0000

    StarRAting

--- The Ultimate React Course/07-usePopcorn/src/components/StarRating.jsx
+++ The Ultimate React Course/07-usePopcorn/src/components/StarRating.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  gap: '16px',
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
 };
 
 const starContainerStyle = {
-  display: 'flex',
-};
-
-const textStyle = {
-  lineHeight: '1',
-  margin: '0',
+  display: "flex",
 };
 
 StarRating.propTypes = {
@@ -23,26 +18,29 @@ StarRating.propTypes = {
   size: PropTypes.number,
   messages: PropTypes.array,
   className: PropTypes.string,
+  onSetRating: PropTypes.func,
 };
 
-const StarRating = ({
+export default function StarRating({
   maxRating = 5,
-  color = '#fcc419',
+  color = "#fcc419",
   size = 48,
-  className = '',
+  className = "",
   messages = [],
   defaultRating = 0,
-}) => {
+  onSetRating,
+}) {
   const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
-  const handleRating = (rating) => {
+  function handleRating(rating) {
     setRating(rating);
-  };
+    onSetRating(rating);
+  }
 
   const textStyle = {
-    lineHeight: '1',
-    margin: '0',
+    lineHeight: "1",
+    margin: "0",
     color,
     fontSize: `${size / 1.5}px`,
   };
@@ -65,20 +63,18 @@ const StarRating = ({
       <p style={textStyle}>
         {messages.length === maxRating
           ? messages[tempRating ? tempRating - 1 : rating - 1]
-          : tempRating || rating || ''}
+          : tempRating || rating || ""}
       </p>
     </div>
   );
-};
+}
 
-export default StarRating;
-
-const Star = ({ onRate, full, onHoverIn, onHoverOut, color, size }) => {
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
   const starStyle = {
     width: `${size}px`,
     height: `${size}px`,
-    display: 'block',
-    cursor: 'pointer',
+    display: "block",
+    cursor: "pointer",
   };
 
   return (
@@ -115,15 +111,7 @@ const Star = ({ onRate, full, onHoverIn, onHoverOut, color, size }) => {
       )}
     </span>
   );
-};
-
-/*
-SPACING SYSTEM (px)
-2 / 4 / 8 / 12 / 16 / 24 / 32 / 40 / 48 / 64 / 80 / 96 / 128
-
-FONT SIZE SYSTEM (px)
-10 / 12 / 14 / 16 / 18 / 20 / 24 / 30 / 36 / 44 /52 / 62 / 74 / 86 / 98
-*/
+}
 
 /*
 FULL STAR
@@ -142,18 +130,6 @@ FULL STAR
 
 EMPTY STAR
 
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="#000"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-  />
-</svg>
+
 
 */
