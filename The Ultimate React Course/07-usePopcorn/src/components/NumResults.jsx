63ee47b7efb925eb69c0f558b13c42a8207c5c62
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-19 03:35:58 +0000

    UsePopCorn

--- The Ultimate React Course/07-usePopcorn/src/components/NumResults.jsx
+++
@@ -1,9 +0,0 @@
-const NumResults = ({ movies }) => {
-  return (
-    <p className="num-results">
-      Found <strong>{movies.length}</strong> results
-    </p>
-  );
-};
-
-export default NumResults;
