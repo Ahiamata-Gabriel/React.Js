58e58b0ce6f30ab27f6fbb70151495cf1ae1f590
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-20 01:39:39 +0000

    Movie Detail

--- The Ultimate React Course/07-usePopcorn-v2/src/components/MovieDetail.jsx
+++ The Ultimate React Course/07-usePopcorn-v2/src/components/MovieDetail.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const MovieDetail = ({ selectedID }) => {
-  return <div className="details">{selectedID}</div>;
+const MovieDetail = ({ selectedID, onCloseMovie }) => {
+  return (
+    <div className="details">
+      <button className="btn-back" onClick={onCloseMovie}>
+        &larr;
+      </button>
+      {selectedID}
+    </div>
+  );
 };
 
 export default MovieDetail;
