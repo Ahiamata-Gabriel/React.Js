9406c6ddac571d51b07369590d7d1a26a9b81fa8
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-20 01:38:31 +0000

    src/components/Movie.jsx

--- The Ultimate React Course/07-usePopcorn-v2/src/components/Movie.jsx
+++ The Ultimate React Course/07-usePopcorn-v2/src/components/Movie.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Movie = ({ movie }) => {
+const Movie = ({ movie, onSelectMovie }) => {
   return (
-    <li>
+    <li onClick={() => onSelectMovie(movie.imdbID)}>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
       <h3>{movie.Title}</h3>
       <div>
