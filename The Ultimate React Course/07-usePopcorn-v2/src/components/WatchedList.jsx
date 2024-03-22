c8b9c631268420c047a0b201f8b1f1f80399c26e
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-22 02:11:30 +0000

    UsepopCorn

--- The Ultimate React Course/07-usePopcorn-v2/src/components/WatchedList.jsx
+++ The Ultimate React Course/07-usePopcorn-v2/src/components/WatchedList.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import WatchedMovie from './WatchedMovie';
+import React from "react";
+import WatchedMovie from "./WatchedMovie";
 
-const WatchedList = ({ watched }) => {
+const WatchedList = ({ watched, onDeleteWatchedMovie }) => {
   return (
     <ul className="list">
       {watched.map((movie) => (
-        <WatchedMovie movie={movie} key={movie.imdbID} />
+        <WatchedMovie
+          movie={movie}
+          key={movie.imdbID}
+          onDeleteWatchedMovie={onDeleteWatchedMovie}
+        />
       ))}
     </ul>
   );
