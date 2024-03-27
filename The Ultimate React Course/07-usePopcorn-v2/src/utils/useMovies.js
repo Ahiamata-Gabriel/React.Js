29f7dada8ed2c9bb1a5a67a4f5267dff68a3cfd6
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-03-27 00:59:27 +0000

    useMovies.js

---
+++ The Ultimate React Course/07-usePopcorn-v2/src/utils/useMovies.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+const KEY = "73c9dba8";
+
+export const useMovies = (query) => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      // callback?.();
+      const controller = new AbortController();
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            {
+              signal: controller.signal,
+            }
+          );
+
+          if (!res.ok) throw new Error("Something went wrong");
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found");
+
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
+  return { movies, isLoading, error };
+};
