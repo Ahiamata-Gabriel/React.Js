e51f7665e8d81b08c0a5c9c9a1181ebab94efc71
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-03 01:56:28 +0000

    QuizApp

---
+++ The Ultimate React Course/08-react-quiz/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import DateCounter from "./components/DateCounter";
+
+function App() {
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  );
+}
+
+export default App;
