d1de0990bee4e00ffa5da0b9f711e5ac4663c143
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-05 01:25:43 +0000

    reducer.js

---
+++ The Ultimate React Course/08-react-quiz/src/utils/reducer.js
@@ -0,0 +1,24 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    default:
+      throw new Error("Action unkown");
+  }
+};
+
+export default reducer;
