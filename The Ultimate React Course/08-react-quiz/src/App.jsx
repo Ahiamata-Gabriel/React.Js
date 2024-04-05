41ec4265bbc7a2f55a6fa84c92821ee48d912aa7
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-05 01:21:21 +0000

    App.jsx

--- The Ultimate React Course/08-react-quiz/src/App.jsx
+++ The Ultimate React Course/08-react-quiz/src/App.jsx
@@ -1,10 +1,45 @@
 import "./App.css";
-import DateCounter from "./components/DateCounter";
+import { useEffect, useReducer } from "react";
+import reducer from "./utils/reducer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/startScreen";
+import Question from "./components/Questions";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status, index } = state;
+  const numberOfQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen
+            numberOfQuestions={numberOfQuestions}
+            dispatch={dispatch}
+          />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
+      </Main>
     </div>
   );
 }
