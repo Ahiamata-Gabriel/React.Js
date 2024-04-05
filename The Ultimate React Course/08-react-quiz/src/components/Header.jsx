38c2f385a27b3408834ea345002725c34167c781
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-05 01:27:35 +0000

    Header.jsx

--- The Ultimate React Course/08-react-quiz/src/components/Header.jsx
+++ The Ultimate React Course/08-react-quiz/src/components/Header.jsx
@@ -1,7 +1,9 @@
+import logo from "../assets/images/logo512.png";
+
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+    <header className="app-header">
+      <img src={logo} alt="React logo" />
       <h1>The React Quiz</h1>
     </header>
   );
