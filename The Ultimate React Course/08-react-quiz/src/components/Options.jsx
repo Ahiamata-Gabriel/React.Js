b0b0cd103fe4bcb59ada26a0c77a6ea64181e83a
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-05 01:28:18 +0000

    Options.jsx

---
+++ The Ultimate React Course/08-react-quiz/src/components/Options.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Options = ({ question }) => {
+  return (
+    <div className="options">
+      {question.options.map((option) => (
+        <button className="btn btn-option" key={option}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Options;
