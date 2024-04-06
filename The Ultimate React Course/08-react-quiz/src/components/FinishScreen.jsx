64e49952ce3a1d032c69c42e8a420305766ee54a
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-06 00:46:32 +0000

    FinishScreen.jsx

---
+++ The Ultimate React Course/08-react-quiz/src/components/FinishScreen.jsx
@@ -0,0 +1,15 @@
+const FinishScreen = ({ points, maxPossiblePoints, highscore }) => {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  return (
+    <>
+      <p className="result">
+        You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+    </>
+  );
+};
+
+export default FinishScreen;
