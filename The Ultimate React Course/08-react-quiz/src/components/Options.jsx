18c332bb43edb81f35d74a96b469ab1e7454a359
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-06 00:47:37 +0000

    Options.jsx

--- The Ultimate React Course/08-react-quiz/src/components/Options.jsx
+++ The Ultimate React Course/08-react-quiz/src/components/Options.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
-const Options = ({ question }) => {
+const Options = ({ question, dispatch, answer }) => {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
