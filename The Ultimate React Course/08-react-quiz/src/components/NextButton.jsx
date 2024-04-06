888dcac2d6a67ee4068a98f7d791c8b278761743
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-06 00:47:07 +0000

    NextButton.jsx

---
+++ The Ultimate React Course/08-react-quiz/src/components/NextButton.jsx
@@ -0,0 +1,25 @@
+const NextButton = ({ dispatch, answer, index, numberOfQuestions }) => {
+  if (answer === null) return null;
+
+  if (index < numberOfQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+
+  if (index === numberOfQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finished
+      </button>
+    );
+};
+
+export default NextButton;
