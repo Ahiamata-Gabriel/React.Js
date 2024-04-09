e602c45d7d391cbae59f9b21863cf1b4802303c2
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-09 00:45:54 +0000

    reducer.js

--- The Ultimate React Course/08-react-quiz/src/utils/reducer.js
+++ The Ultimate React Course/08-react-quiz/src/utils/reducer.js
@@ -1,3 +1,5 @@
+const SECS_PER_QUESTION = 30;
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "dataRecieved":
@@ -15,10 +17,10 @@ const reducer = (state, action) => {
       return {
         ...state,
         status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
     case "newAnswer":
       const question = state.questions.at(state.index);
-
       return {
         ...state,
         answer: action.payload,
@@ -35,6 +37,22 @@ const reducer = (state, action) => {
         status: "finished",
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return {
+        ...state,
+        index: 0,
+        answer: null,
+        points: 0,
+        highscore: 0,
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+        status: "ready",
+      };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
       };
     default:
       throw new Error("Action unkown");
