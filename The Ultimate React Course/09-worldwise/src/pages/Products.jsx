f4fc12024406c9b1e6c6f6dcd043e9b2b1359e89
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-21 02:12:04 +0000

    Products.jsx

---
+++ The Ultimate React Course/09-worldwise/src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../components/PageNav";
+const Products = () => {
+  return (
+    <div>
+      <PageNav />
+      <h1>Products</h1>
+    </div>
+  );
+};
+
+export default Products;
