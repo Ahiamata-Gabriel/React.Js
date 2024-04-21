165deb54efce7fde62d13c69e242a61c36f06f7a
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-21 02:09:35 +0000

    Home.jsx

---
+++ The Ultimate React Course/09-worldwise/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+const Home = () => {
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+    </div>
+  );
+};
+
+export default Home;
