87e5b28588e565671d86164db63bd8a6460db7c2
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-21 02:11:31 +0000

    Pricing.jsx

---
+++ The Ultimate React Course/09-worldwise/src/pages/Pricing.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../components/PageNav";
+const Pricing = () => {
+  return (
+    <div>
+      <PageNav />
+      <h1> Pricing</h1>
+    </div>
+  );
+};
+
+export default Pricing;
