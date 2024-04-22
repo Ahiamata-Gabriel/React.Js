e6aed7865e22afc4a2e8ebf4988de6d030419966
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-22 08:48:54 +0000

    App.jsx

--- The Ultimate React Course/09-worldwise/src/App.jsx
+++ The Ultimate React Course/09-worldwise/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Pricing from "./pages/Pricing";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="product" element={<Products />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
