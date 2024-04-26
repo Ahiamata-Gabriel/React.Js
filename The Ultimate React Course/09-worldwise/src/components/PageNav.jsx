2f949fbbc8f5d4a58652436b425717b78b38a915
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-04-26 02:05:52 +0000

    PageNav.jsx

---
+++ The Ultimate React Course/09-worldwise/src/components/PageNav.jsx
@@ -0,0 +1,26 @@
+import styles from "../components/PageNav.module.css";
+import Logo from "./Logo";
+import { NavLink } from "react-router-dom";
+
+const PageNav = () => {
+  return (
+    <nav className={styles.nav}>
+      <Logo />
+      <ul>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/products">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default PageNav;
