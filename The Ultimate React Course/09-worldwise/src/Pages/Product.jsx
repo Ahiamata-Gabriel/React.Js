dcd45ae5b3feb97edbd99c4b2b3150ed08500a06
Ahiamata-Gabriel <ahimatamawuli@gmail.com>
2024-04-30 00:22:15 +0000

    update

---
+++ The Ultimate React Course/09-worldwise/src/Pages/Product.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+const Product = () => {
+  return (
+    <div>
+      <PageNav />
+      <h1>Product</h1>
+    </div>
+  );
+};
+
+export default Product;
---
+++ The Ultimate React Course/09-worldwise/src/components/PageNav.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+const PageNav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/products">Products</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default PageNav;
