237477688a4fef306d78644cd425093eacf14698
Ahiamata-Gabriel <ahimatamawuli@gmail.com>
2024-04-30 00:28:30 +0000

    update

--- The Ultimate React Course/09-worldwise/src/App.jsx
+++ The Ultimate React Course/09-worldwise/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
+        <Route path="products" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
