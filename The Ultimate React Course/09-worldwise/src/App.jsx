11d22f7872a3d0e84471aac3257254a0e8cdbb20
Ahiamata-Gabriel <ahimatamawuli@gmail.com>
2024-04-30 03:21:23 +0000

    update

--- The Ultimate React Course/09-worldwise/src/App.jsx
+++ The Ultimate React Course/09-worldwise/src/App.jsx
@@ -7,8 +7,31 @@ import PageNotFound from "./Pages/PageNotFound";
 import AppLayout from "./Pages/AppLayout";
 import Login from "./Pages/Login";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [loading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        console.log(data);
+      } catch (error) {
+        alert("som went wrong");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
