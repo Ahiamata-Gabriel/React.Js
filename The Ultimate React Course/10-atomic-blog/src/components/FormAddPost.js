1f2a02b3970d447da291c86e304b541faeb88f28
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-02 01:39:19 +0000

    FormAddPost.js

---
+++ The Ultimate React Course/10-atomic-blog/src/components/FormAddPost.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export function FormAddPost({ onAddPost }) {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    if (!body || !title) return;
+    onAddPost({ title, body });
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Post title"
+      />
+      <textarea
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        placeholder="Post body"
+      />
+      <button>Add post</button>
+    </form>
+  );
+}
