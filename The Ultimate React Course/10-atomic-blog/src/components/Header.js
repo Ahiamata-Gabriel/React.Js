2b140a0cf699c0b5b0845750bb8de18f3f79a435
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-02 01:40:13 +0000

    Header.js

---
+++ The Ultimate React Course/10-atomic-blog/src/components/Header.js
@@ -0,0 +1,22 @@
+import Results from "./Results";
+import SearchPosts from "./SearchPosts";
+
+function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
+  return (
+    <header>
+      <h1>
+        <span>⚛️</span>The Atomic Blog
+      </h1>
+      <div>
+        <Results posts={posts} />
+        <SearchPosts
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+        <button onClick={onClearPosts}>Clear posts</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
