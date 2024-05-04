527d15bedab3022b6070876172e7d6624cce139e
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-04 01:23:29 +0000

    V2

---
+++ The Ultimate React Course/10-atomic-blog-v2/src/components/Header.js
@@ -0,0 +1,22 @@
+import Results from "./Results";
+import SearchPosts from "./SearchPosts";
+import { usePosts } from "../context/PostContext";
+
+function Header() {
+  const { onClearPosts } = usePosts();
+
+  return (
+    <header>
+      <h1>
+        <span>⚛️</span>The Atomic Blog
+      </h1>
+      <div>
+        <Results />
+        <SearchPosts />
+        <button onClick={onClearPosts}>Clear posts</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
