5d6294964fb3e0446c1d9be42e0974676795723f
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-05 00:53:04 +0000

    CityList.jsx

--- The Ultimate React Course/09-worldwise/src/components/CityList.jsx
+++ The Ultimate React Course/09-worldwise/src/components/CityList.jsx
@@ -2,8 +2,11 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
+import { useCities } from "../contexts/CitiesContext";
 
-const CityList = ({ cities, isLoading }) => {
+const CityList = () => {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
