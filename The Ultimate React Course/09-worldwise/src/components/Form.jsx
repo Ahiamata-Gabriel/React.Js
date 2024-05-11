7eace778e0f4d52c19fc26f0d9908e202ea27cf8
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-11 20:14:48 +0000

    Form.jsx

--- The Ultimate React Course/09-worldwise/src/components/Form.jsx
+++ The Ultimate React Course/09-worldwise/src/components/Form.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Button from "./Button";
 import styles from "./Form.module.css";
-import { useNavigate } from "react-router-dom";
+import BackButton from "./BackButton";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -14,10 +14,8 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
-  const navigate = useNavigate();
-
   const [cityName, setCityName] = useState("");
-  const [country, setCountry] = useState("");
+  //const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
 
@@ -53,15 +51,7 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type="primary">Add</Button>
-        <Button
-          type="back"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(-1);
-          }}
-        >
-          &larr; Back
-        </Button>
+        <BackButton />
       </div>
     </form>
   );
