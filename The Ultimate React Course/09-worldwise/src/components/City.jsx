08134cc27021a45f23d8f76ea8ad5dc4137c885c
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-11 20:13:34 +0000

    City.jsx

--- The Ultimate React Course/09-worldwise/src/components/City.jsx
+++ The Ultimate React Course/09-worldwise/src/components/City.jsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import styles from "./City.module.css";
 import { useParams } from "react-router-dom";
 import { useCities } from "../contexts/CitiesContext";
+import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -13,9 +15,7 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const { getCity, currentCity } = useCities();
-
-  console.log(currentCity);
+  const { getCity, currentCity, isLoading } = useCities();
 
   useEffect(
     function () {
@@ -25,6 +25,8 @@ function City() {
   );
 
   const { cityName, emoji, date, notes } = currentCity;
+
+  if (isLoading) return <Spinner />;
 
   return (
     <div className={styles.city}>
@@ -58,7 +60,9 @@ function City() {
         </a>
       </div>
 
-      <div></div>
+      <div>
+        <BackButton />
+      </div>
     </div>
   );
 }
