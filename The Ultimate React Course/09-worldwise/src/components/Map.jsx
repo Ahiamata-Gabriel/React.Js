31e0bdd2120f78ddbb198bf83bb09e79c2503f13
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-13 06:48:31 +0000

    update

--- The Ultimate React Course/09-worldwise/src/components/Map.jsx
+++ The Ultimate React Course/09-worldwise/src/components/Map.jsx
@@ -1,22 +1,48 @@
+import { useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useCities } from "../contexts/CitiesContext";
 import styles from "./Map.module.css";
-const Map = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
 
+const Map = () => {
   const navigate = useNavigate();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
 
   return (
     <div
       className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
+      // onClick={() => {
+      //   navigate("form");
+      // }}
     >
-      <h1>
-        {lat}, {lng}
-      </h1>
+      <MapContainer
+        className={styles.map}
+        center={[mapLat, mapLng]}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            // position={[lat, lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 };
