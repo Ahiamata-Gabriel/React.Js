bdbd7e578ce4135c04f7ec109b679ff49a177672
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-21 01:07:25 +0000

    Map.jsx

--- The Ultimate React Course/09-worldwise/src/components/Map.jsx
+++ The Ultimate React Course/09-worldwise/src/components/Map.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
   MapContainer,
   TileLayer,
