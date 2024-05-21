be0eaee95830161c9a9c497359da70de05366b42
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-21 01:55:47 +0000

    Form.jsx

--- The Ultimate React Course/09-worldwise/src/components/Form.jsx
+++ The Ultimate React Course/09-worldwise/src/components/Form.jsx
@@ -4,6 +4,8 @@ import Button from "./Button";
 import styles from "./Form.module.css";
 import BackButton from "./BackButton";
 import { useUrlPosition } from "../hooks/useUrlPosition";
+import Message from "./Message";
+import Spinner from "./Spinner";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -23,23 +25,29 @@ function Form() {
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
   const [emoji, setEmoji] = useState("");
+  const [geocodingError, setGeocodingError] = useState("");
 
   useEffect(
     function () {
       async function fetchCityData() {
         try {
           setIsLoadingGeocoding(true);
+          setGeocodingError("");
           const res = await fetch(
             `${BASE_URL}?latitude=${lat}&longitude=${lng}`
           );
           const data = await res.json();
-          console.log(data);
+
+          if (!data.countryCode)
+            throw new Error("No country code found click somehere else");
+
           setCityName(data.city || data.locality || "");
           setCountry(data.countryName);
           setEmoji(convertToEmoji(data.countryCode));
         } catch (error) {
+          setGeocodingError(error.message);
         } finally {
-          setIsLoadingGeocoding(true);
+          setIsLoadingGeocoding(false);
         }
       }
 
@@ -47,6 +55,10 @@ function Form() {
     },
     [lat, lng]
   );
+
+  if (isLoadingGeocoding) return <Spinner />;
+
+  if (geocodingError) return <Message message={geocodingError} />;
 
   return (
     <form className={styles.form}>
