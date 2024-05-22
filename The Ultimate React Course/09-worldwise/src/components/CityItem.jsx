d7570651f284ffcfc5bfb246145877f4df2cdee1
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-22 00:19:17 +0000

    CityItem.jsx

--- The Ultimate React Course/09-worldwise/src/components/CityItem.jsx
+++ The Ultimate React Course/09-worldwise/src/components/CityItem.jsx
@@ -3,8 +3,13 @@ import styles from "./CityItem.module.css";
 import { useCities } from "../contexts/CitiesContext";
 
 const CityItem = ({ city }) => {
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    deleteCity(id);
+  };
 
   const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -26,7 +31,9 @@ const CityItem = ({ city }) => {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
       </Link>
     </li>
   );
