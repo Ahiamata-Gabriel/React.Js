d123834dba3a7cd6166f2afad858d11a7e3be4e1
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-25 02:13:54 +0000

    AppLayout.jsx

--- The Ultimate React Course/09-worldwise/src/Pages/AppLayout.jsx
+++ The Ultimate React Course/09-worldwise/src/Pages/AppLayout.jsx
@@ -1,5 +1,6 @@
 import Map from "../components/Map";
 import SideBar from "../components/SideBar";
+import User from "../components/User";
 import styles from "./AppLayout.module.css";
 
 const AppLayout = () => {
@@ -7,6 +8,7 @@ const AppLayout = () => {
     <div className={styles.app}>
       <SideBar />
       <Map />
+      <User />
     </div>
   );
 };
