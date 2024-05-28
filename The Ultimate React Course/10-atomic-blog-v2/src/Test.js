303cc4257adbe0f463435efd8e36a07e45881b95
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-28 00:30:39 +0000

    Test.js

--- The Ultimate React Course/10-atomic-blog-v2/src/Test.js
+++ The Ultimate React Course/10-atomic-blog-v2/src/Test.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 function SlowComponent() {
   // If this is too slow on your maching, reduce the `length`
   const words = Array.from({ length: 100_000 }, () => "WORD");
+
   return (
     <ul>
       {words.map((word, i) => (
