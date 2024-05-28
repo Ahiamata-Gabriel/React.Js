acad0206f7b7448d86d1619a821b0317e3d47c74
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-05-28 00:27:59 +0000

    List.js

--- The Ultimate React Course/10-atomic-blog-v2/src/components/List.js
+++ The Ultimate React Course/10-atomic-blog-v2/src/components/List.js
@@ -1,12 +1,18 @@
+import Test from "../Test";
+
 export function List({ posts }) {
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+
+      {/* <Test /> */}
+    </>
   );
 }
