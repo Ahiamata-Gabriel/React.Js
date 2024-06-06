c25e63163ab5c76fbb321f3958851f1371bf6488
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-06 00:29:06 +0000

    store.js

---
+++ The Ultimate React Course/12-redux-intro/src/store.js
@@ -0,0 +1,30 @@
+const initialStae = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+const reducer = (state = initialStae, action) => {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: state.loan + action.payload,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
+};
