1fd0ba54142e879260c9219bb2029cef263eed2b
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-08 01:28:26 +0000

    App.jsx

--- The Ultimate React Course/12-redux-intro/src/App.jsx
+++ The Ultimate React Course/12-redux-intro/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
-
+import AccountOperations from "./componenets/AccountOperations";
+import Customer from "./componenets/CreateCustomer";
+import BalanceDisplay from "./componenets/BalanceDisplay";
+import CreateCustomer from "./componenets/CreateCustomer";
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      <CreateCustomer />
+      <Customer />
+      <AccountOperations />
+      <BalanceDisplay />
+    </div>
+  );
 }
 
 export default App;
