da8f6984376b01f9412ecedc2c3f9f124fbfad1c
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-19 15:05:40 +0000

    Customer.jsx

--- The Ultimate React Course/12-redux-intro/src/features/customers/Customer.jsx
+++ The Ultimate React Course/12-redux-intro/src/features/customers/Customer.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 
 function Customer() {
-  const customer = useSelector((store) => store.customer.fullName);
+  const customer = useSelector((store) => store.customer.nationalID);
 
   console.log(customer);
 
