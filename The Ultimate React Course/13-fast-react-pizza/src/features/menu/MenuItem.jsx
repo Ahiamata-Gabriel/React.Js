3998b7d767f9aecf7e09b246aa9713e33ba34a7f
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-25 00:47:44 +0000

    MenuItem.jsx

---
+++ The Ultimate React Course/13-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+
+import { formatCurrency } from "../../utils/helpers";
+
+/* eslint-disable no-unused-vars */
+function MenuItem({ pizza }) {
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  return (
+    <li>
+      <img src={imageUrl} alt={name} />
+      <div>
+        <p>{name}</p>
+        <p>{ingredients.join(", ")}</p>
+        <div>
+          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default MenuItem;
