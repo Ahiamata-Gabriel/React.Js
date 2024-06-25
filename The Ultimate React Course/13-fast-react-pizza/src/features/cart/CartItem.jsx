5da09b4d15c25ccb7fea6fc5ef49d98f947e6284
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-25 00:45:57 +0000

    CartItem.jsx

---
+++ The Ultimate React Course/13-fast-react-pizza/src/features/cart/CartItem.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+
+import { formatCurrency } from "../../utils/helpers";
+
+/* eslint-disable react/prop-types */
+function CartItem({ item }) {
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  return (
+    <li>
+      <p>
+        {quantity}&times; {name}
+      </p>
+      <div>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default CartItem;
