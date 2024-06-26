ea1b6b28e0b9a2d686a6bad47bb647ec08f96648
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-26 00:40:13 +0000

    App.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/App.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/App.jsx
@@ -1,9 +1,34 @@
 /* eslint-disable no-unused-vars */
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
+]);
 
 function App() {
-  let x = 100;
-  return <div>HHHHH {x}</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
