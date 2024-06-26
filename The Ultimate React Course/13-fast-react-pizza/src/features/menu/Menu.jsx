4ef8e8641b12fd28ef4c5dc1105bcd306493bb77
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-26 00:35:18 +0000

    Menu.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/features/menu/Menu.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,5 +1,24 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable no-unused-vars */
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
