a115d61ac04854c164bd45e1062c091e8016feda
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-26 00:38:58 +0000

    Error.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/ui/Error.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/Error.jsx
@@ -1,15 +1,17 @@
-import { useNavigate } from 'react-router-dom';
+/* eslint-disable no-unused-vars */
+import { useNavigate, useRouteError } from "react-router-dom";
 
-function NotFound() {
+function Error() {
   const navigate = useNavigate();
+  const error = useRouteError();
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <p>{error.data}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
 }
 
-export default NotFound;
+export default Error;
