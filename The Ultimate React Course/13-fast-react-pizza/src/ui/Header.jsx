fb2ddd9f91f9f20aa3a828a9ac34ce92f39b6f21
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-26 00:39:15 +0000

    Header.jsx

---
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Fast Pizza Company</Link>
+      <p>Bang Bang !!</p>
+    </header>
+  );
+};
+
+export default Header;
