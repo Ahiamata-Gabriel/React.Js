1858f37b0f18e180190b3494015a6121bde05339
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-26 00:40:29 +0000

    AppCopy.jsx

---
+++ The Ultimate React Course/13-fast-react-pizza/src/AppCopy.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import "./App.css";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<AppLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/Cart" element={<Cart />} />
+      <Route path="/CreateOrder" element={<CreateOrder />} />
+      <Route path="/order/:orderId" element={<Order />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
