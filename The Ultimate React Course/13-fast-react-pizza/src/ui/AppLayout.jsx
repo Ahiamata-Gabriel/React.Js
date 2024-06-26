d0b2ac1fc1103d7d0dd0ae7366f4fddc861cca77
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-26 00:38:41 +0000

    AppLayout.jsx

---
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
+
+const AppLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
