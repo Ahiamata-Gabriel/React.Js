8e6f754a67a7786004bc3ea413c293132759fbbd
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-28 01:18:49 +0000

    App.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/App.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -19,10 +19,16 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
