b1cff8e156ec5bc843551e94d296bd652fdd8a9b
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-28 01:18:08 +0000

    Header.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/ui/Header.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/Header.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 const Header = () => {
   return (
     <header>
       <Link to="/">Fast Pizza Company</Link>
-      <p>Bang Bang !!</p>
+      <SearchOrder />
     </header>
   );
 };
