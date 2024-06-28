831cb7af8b0f21bed5510c5cc799039f613c4da3
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-28 01:15:25 +0000

    SearchOrder.jsx

---
+++ The Ultimate React Course/13-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const SearchOrder = () => {
+  const [query, setQuery] = useState("");
+
+  const navingate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!query) return;
+    navingate(`/order/${query}`);
+    setQuery("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+};
+
+export default SearchOrder;
