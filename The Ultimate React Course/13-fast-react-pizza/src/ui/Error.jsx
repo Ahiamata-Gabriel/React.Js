b135f7d65e64f5191a3e99166f8ecfa7e1708e43
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-28 01:16:21 +0000

    Error.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/ui/Error.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/Error.jsx
@@ -8,7 +8,7 @@ function Error() {
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>{error.data}</p>
+      <p>{error.data || error.message}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
