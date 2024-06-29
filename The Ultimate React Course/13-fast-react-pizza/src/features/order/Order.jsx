cc6bd57871d6a7ce84f6470bd77464fb90108743
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-29 02:14:21 +0000

    Order.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/features/order/Order.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/features/order/Order.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 // Test ID: IIDSAT
 
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
@@ -10,44 +10,8 @@ import {
   formatDate,
 } from "../../utils/helpers";
 
-// const order = {
-//   id: "ABCDEF",
-//   customer: "Jonas",
-//   phone: "123456789",
-//   address: "Arroios, Lisbon , Portugal",
-//   priority: true,
-//   estimatedDelivery: "2027-04-25T10:00:00",
-//   cart: [
-//     {
-//       pizzaId: 7,
-//       name: "Napoli",
-//       quantity: 3,
-//       unitPrice: 16,
-//       totalPrice: 48,
-//     },
-//     {
-//       pizzaId: 5,
-//       name: "Diavola",
-//       quantity: 2,
-//       unitPrice: 16,
-//       totalPrice: 32,
-//     },
-//     {
-//       pizzaId: 3,
-//       name: "Romana",
-//       quantity: 1,
-//       unitPrice: 15,
-//       totalPrice: 15,
-//     },
-//   ],
-//   position: "-9.000,38.000",
-//   orderPrice: 95,
-//   priorityPrice: 19,
-// };
-
 function Order() {
   const order = useLoaderData();
-  let { orderId } = useParams();
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
