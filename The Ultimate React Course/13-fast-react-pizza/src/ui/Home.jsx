f896a494ead76e56f7df5954e4032a093f422cc6
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-03 00:46:48 +0000

    Home

--- The Ultimate React Course/13-fast-react-pizza/src/ui/Home.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/Home.jsx
@@ -1,11 +1,17 @@
+import CreateUser from "../features/user/CreateUser";
+
 function Home() {
   return (
-    <div>
-      <h1 className="text-xl">
+    <div className="my-10 text-center sm:my-16">
+      <h1 className="mb-8 text-xl font-semibold">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
+
+      <CreateUser />
     </div>
   );
 }
