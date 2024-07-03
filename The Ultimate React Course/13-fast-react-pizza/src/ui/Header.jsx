dbe620e8dd8b3c00f466b9c2e57e3afae9fd5bae
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-03 00:45:30 +0000

    Header.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/ui/Header.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Fast Pizza Company</Link>
+    <header className="border-b border-stone-200 bg-yellow-500 px-3 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast Pizza Company
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 };
