f6d8f05525c34479450bf2d986e247da3bdbf3d0
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-06 01:47:14 +0000

    MenuItem.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,18 +1,30 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+      />
+      <div className="flex grow flex-col">
         <p>{name}</p>
         <p>{ingredients.join(", ")}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        <div className="mt-auto flex items-center justify-between">
+          {!soldOut ? (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              {formatCurrency(unitPrice)}
+            </p>
+          ) : (
+            <p>Sold out</p>
+          )}
+          <Button type="small"> Add to cart</Button>
         </div>
       </div>
     </li>
