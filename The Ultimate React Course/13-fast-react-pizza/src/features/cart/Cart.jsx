61ad0376a8f7cab178b4eaa8228bc2f7ebe0462b
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-06 01:45:30 +0000

    Cart.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/features/cart/Cart.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
-import { Link } from "react-router-dom";
+import LinkButton from "../../ui/LinkButton";
+import Button from "../../ui/Button";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -29,18 +31,22 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <Link
-        to="/menu"
-        className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
-      >
-        &larr; Back to menu
-      </Link>
+    <div className="px-4 py-3">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
-        <Link to="/order/new">Order pizzas</Link>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+
         <button>Clear cart</button>
       </div>
     </div>
