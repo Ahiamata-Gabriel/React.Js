24c0107bcdb03c964e9ee4d3b0706ab2926ef5b9
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-06 01:49:18 +0000

    apiRestaurant.js

--- The Ultimate React Course/13-fast-react-pizza/src/services/apiRestaurant.js
+++ The Ultimate React Course/13-fast-react-pizza/src/services/apiRestaurant.js
@@ -1,7 +1,7 @@
 const API_URL = "https://react-fast-pizza-api.onrender.com/api";
 
 export async function getMenu() {
-  const res = await fetch(`${API_URL}/mnu`);
+  const res = await fetch(`${API_URL}/menu`);
 
   // fetch won't throw error on 400 errors (e.g. when URL is wrong), so we need to do it manually. This will then go into the catch block, where the message is set
   if (!res.ok) throw Error("Failed getting menu");
