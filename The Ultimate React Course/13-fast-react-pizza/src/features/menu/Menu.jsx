de05d94029eef2f9bfb82cc2e43fb7a561798b25
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-06 01:46:49 +0000

    Menu.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/features/menu/Menu.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/features/menu/Menu.jsx
@@ -8,7 +8,7 @@ function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem key={pizza.id} pizza={pizza} />
       ))}
