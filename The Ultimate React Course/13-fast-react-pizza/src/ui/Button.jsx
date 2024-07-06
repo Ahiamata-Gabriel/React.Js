22abb30fd152c3b01efafcde1027859b0005f641
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-06 01:50:21 +0000

    Button.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/ui/Button.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/Button.jsx
@@ -1,11 +1,25 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-const Button = ({ children, disabled }) => {
+import { Link } from "react-router-dom";
+
+const Button = ({ children, disabled, to, type }) => {
+  const base =
+    "my-2 inline-block rounded-full bg-yellow-500 font-semibold uppercase tracking-wide text-stone-800 transition-colors hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed ";
+
+  const styles = {
+    primary: base + " px-4 py-3 mb:px-6 mb:py-4",
+    small: base + " py-2 mb:px-5 mb:py-2.5 px-4 text-xs",
+  };
+
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+
   return (
-    <button
-      disabled={disabled}
-      className="my-2 inline-block rounded-full bg-yellow-500 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed"
-    >
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
