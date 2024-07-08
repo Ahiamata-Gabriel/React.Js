d813bbdeb32457854f7c3bb2a508ccfced0dac8b
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-08 00:25:16 +0000

    Tailwind

--- The Ultimate React Course/13-fast-react-pizza/tailwind.config.js
+++ The Ultimate React Course/13-fast-react-pizza/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
   },
   plugins: [],
 };
