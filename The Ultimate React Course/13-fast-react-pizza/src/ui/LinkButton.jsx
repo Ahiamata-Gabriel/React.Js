c15b56c7dd1e05f897261bad20c48d026fd4e960
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-08 00:23:37 +0000

    LinkButton.jsx

---
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/LinkButton.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { Link, useNavigate } from "react-router-dom";
+
+const LinkButton = ({ children, to }) => {
+  const navigate = useNavigate();
+  const className = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
+
+  if (to === "-1")
+    return (
+      <button className={className} onClick={() => navigate(-1)}>
+        &larr; Go back
+      </button>
+    );
+
+  return (
+    <Link to={to} className={className}>
+      {children}
+    </Link>
+  );
+};
+
+export default LinkButton;
