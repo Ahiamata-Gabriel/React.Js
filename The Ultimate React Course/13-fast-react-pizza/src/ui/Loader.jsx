2c6e98e77dfbc1c2f8820c660672af409eb23651
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-08 00:24:17 +0000

    Loader.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/ui/Loader.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/Loader.jsx
@@ -1,5 +1,9 @@
 const Loader = () => {
-  return <div className="loader"></div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-[3px]">
+      <div className="loader"></div>;
+    </div>
+  );
 };
 
 export default Loader;
