058a1f33df873b7700183ae354aff990289ccdd1
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-09 00:24:38 +0000

    Concluded with tailwind

--- The Ultimate React Course/13-fast-react-pizza/src/ui/Button.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/ui/Button.jsx
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom";
 
 const Button = ({ children, disabled, to, type }) => {
   const base =
-    "my-2 inline-block rounded-full bg-yellow-500 font-semibold uppercase tracking-wide text-stone-800 transition-colors hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed ";
+    "my-2 inline-block text-sm rounded-full bg-yellow-500 font-semibold uppercase tracking-wide text-stone-800 transition-colors hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed ";
 
   const styles = {
     primary: base + " px-4 py-3 mb:px-6 mb:py-4",
     small: base + " py-2 mb:px-5 mb:py-2.5 px-4 text-xs",
+    secondary:
+      "my-2 inline-block text-sm px-4 py-2.5 mb:px-6 mb:py-3.5 rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 hover:text-stone-800 focus:text-stone-800 transition-colors hover:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-1 disabled:cursor-not-allowed ",
   };
 
   if (to)
