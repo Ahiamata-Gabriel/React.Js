482393e0133f88b6ce1f604e5f3af0aafe8b9a8c
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-11 01:39:17 +0000

    MenuItem.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,10 +1,24 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newItem));
+  };
 
   return (
     <li className="flex gap-4 py-2">
@@ -24,7 +38,11 @@ function MenuItem({ pizza }) {
           ) : (
             <p>Sold out</p>
           )}
-          <Button type="small"> Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
