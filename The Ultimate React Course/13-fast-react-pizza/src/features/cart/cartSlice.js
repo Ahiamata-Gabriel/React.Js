3882e3b74e8ab636574c02b1f324e591a7d19e43
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-11 01:38:47 +0000

    cartSlice.js

---
+++ The Ultimate React Course/13-fast-react-pizza/src/features/cart/cartSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  // cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      //payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // paylod = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      // paylod = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    decreaseItemQuantity(state, action) {
+      // paylod = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    clearItem(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearItem,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
