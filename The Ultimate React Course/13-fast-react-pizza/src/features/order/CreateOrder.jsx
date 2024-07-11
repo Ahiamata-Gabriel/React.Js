8b73b1d2c3e946361f4022a111983eae7094719a
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-11 01:39:54 +0000

    CreateOrder.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
-/* eslint-disable no-unused-vars */
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -40,6 +40,7 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+  const username = useSelector((state) => state.user.username);
 
   const formErrors = useActionData();
 
@@ -54,7 +55,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            name="customer"
+            required
+            defaultValue={username}
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
