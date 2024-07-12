6e0ba7f97de1589f9e664f71daaa559e1927f609
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-12 00:33:44 +0000

    CartOverview.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/features/cart/CartOverview.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable no-unused-vars */
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function CartOverview() {
+  const x = useSelector((state) => state.cart.cart);
+
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
