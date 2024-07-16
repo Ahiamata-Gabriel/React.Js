88ad2b9fc62545fba0dd7ddcc4792d5ceb6a699d
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-16 11:18:55 +0000

    CartItem.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/features/cart/CartItem.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/features/cart/CartItem.jsx
@@ -1,8 +1,12 @@
-import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 import { formatCurrency } from "../../utils/helpers";
+import { getCurrentQuantityById } from "./cartSlice";
+import DeleteItem from "./DeleteItem";
+import UpdateItemQuantity from "./UpdateItemQuantity";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -11,7 +15,11 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <UpdateItemQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
+        />
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
