8ceaabb00bb7b0f104f1d3a82d458abaf9d062cb
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-16 11:21:04 +0000

    cartSlice.js

--- The Ultimate React Course/13-fast-react-pizza/src/features/cart/cartSlice.js
+++ The Ultimate React Course/13-fast-react-pizza/src/features/cart/cartSlice.js
@@ -41,8 +41,11 @@ const cartSlice = createSlice({
     decreaseItemQuantity(state, action) {
       // paylod = pizzaId
       const item = state.cart.find((item) => item.pizzaId === action.payload);
+
       item.quantity--;
       item.totalPrice = item.unitPrice * item.quantity;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearItem(state) {
       state.cart = [];
@@ -60,8 +63,26 @@ export const {
 
 export default cartSlice.reducer;
 
+export const getCart = (state) => state.cart.cart;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
+// function getCurrentQuantityById(id) {
+//   return function (state) {
+//     let item = state.cart.cart.find(function (item) {
+//       return item.pizzaId === id;
+//     });
+//     return item ? item.quantity : 0;
+//   };
+// }
+
+// Having these selectors here can cause performance issues
+// in larger applications. check out the 'reselect' library to
+// help optimize these selectors
