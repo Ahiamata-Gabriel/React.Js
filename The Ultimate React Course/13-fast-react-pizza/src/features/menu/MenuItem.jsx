bea757001829a4551e79656ec2e4b8f983186e00
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-17 00:11:40 +0000

    MenuItem.jsx

--- The Ultimate React Course/13-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ The Ultimate React Course/13-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,12 +1,16 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
-import { addItem } from "../cart/cartSlice";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
-
   const dispatch = useDispatch();
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
 
   const handleAddToCart = () => {
     const newItem = {
@@ -38,7 +42,16 @@ function MenuItem({ pizza }) {
           ) : (
             <p>Sold out</p>
           )}
-          {!soldOut && (
+          {isInCart && (
+            <div className="flex items-center gap-3">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
