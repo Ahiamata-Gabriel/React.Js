436088c42ae004f93c27f60d45fc881114b81b1b
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-18 00:20:39 +0000

    UpdateOrder.jsx

---
+++ The Ultimate React Course/13-fast-react-pizza/src/features/order/UpdateOrder.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useFetcher } from "react-router-dom";
+import Button from "../../ui/Button";
+import { updateOrder } from "../../services/apiRestaurant";
+
+const UpdateOrder = ({ order }) => {
+  const fetcher = useFetcher();
+
+  return (
+    <fetcher.Form method="PATCH" className="text-right">
+      <Button type="primary">UpdateOrder</Button>
+    </fetcher.Form>
+  );
+};
+
+export default UpdateOrder;
+
+export async function action({ request, params }) {
+  const data = { priority: true };
+  await updateOrder(params.orderId, data);
+  return null;
+}
