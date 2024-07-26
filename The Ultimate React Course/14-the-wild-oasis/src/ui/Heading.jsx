56142845da7a9a819bd840da2c9a7913a3f08730
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-26 00:44:44 +0000

    ui

---
+++ The Ultimate React Course/14-the-wild-oasis/src/ui/Heading.jsx
@@ -0,0 +1,12 @@
+import styled, { css } from "styled-components";
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+`;
+
+export default Heading;
