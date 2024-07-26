2cccbd22dc1fdcac353db313fd9968b7c7f1138a
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-26 00:45:43 +0000

    Introducing the wild Oasis

--- The Ultimate React Course/14-the-wild-oasis/src/App.jsx
+++ The Ultimate React Course/14-the-wild-oasis/src/App.jsx
@@ -1,8 +1,45 @@
-/* eslint-disable no-unused-vars */
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+  padding: 20px;
+`;
 
 function App() {
-  return <h1> hello</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+
+            <div>
+              <Heading as="h2">Check in and Out</Heading>
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
