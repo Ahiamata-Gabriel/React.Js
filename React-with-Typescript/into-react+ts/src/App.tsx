f5096204c04bdbc1513c7c58b785e65f7ec782ad
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-07-31 00:23:24 +0000

    App.tsx

--- React-with-Typescript/into-react+ts/src/App.tsx
+++ React-with-Typescript/into-react+ts/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import "./App.css";
+import Header from "./components/Header";
+import goalsImg from "./assets/goals.jpg";
+import { useState } from "react";
+import CourseGoalList from "./components/CourseGoalList";
+import NewGoal from "./components/NewGoal";
+
+export type CourseGoal = {
+  id: number;
+  title: string;
+  description: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal(goal: string, summary: string) {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: goal,
+        description: summary,
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
+  function handleDeleteGoal(goalId: number) {
+    setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== goalId));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <Header image={{ src: goalsImg, alt: "A list of Goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <NewGoal onAddGoal={handleAddGoal} />
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
+    </main>
+  );
 }
 
-export default App
+export default App;
