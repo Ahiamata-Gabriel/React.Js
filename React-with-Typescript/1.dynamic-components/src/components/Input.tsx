830b4cd89b11fefe6d496b247bf3b2ad68ec6ce0
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-02 00:18:10 +0000

    Input.tsx

--- React-with-Typescript/1.dynamic-components/src/components/Input.tsx
+++ React-with-Typescript/1.dynamic-components/src/components/Input.tsx
@@ -1,17 +1,20 @@
-import { ComponentPropsWithoutRef } from "react";
+import { forwardRef, type ComponentPropsWithoutRef } from "react";
 
 type InputProps = {
   label: string;
   id: string;
 } & ComponentPropsWithoutRef<"input">;
 
-function Input({ label, id, ...props }: InputProps) {
+const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
+  { label, id, ...props },
+  ref
+) {
   return (
     <p>
       <label htmlFor={id}>{label}</label>
-      <input id={id} {...props} />
+      <input id={id} name={id} {...props} ref={ref} />
     </p>
   );
-}
+});
 
 export default Input;
