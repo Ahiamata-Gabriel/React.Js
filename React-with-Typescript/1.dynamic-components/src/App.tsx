6a8c84176580ebb58348bdf0144ed8aa444f99e6
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-02 00:18:41 +0000

    Ts

--- React-with-Typescript/1.dynamic-components/src/App.tsx
+++ React-with-Typescript/1.dynamic-components/src/App.tsx
@@ -1,10 +1,22 @@
 import Input from "./components/Input";
+import Form from "./components/Form";
 
 function App() {
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+  }
+
   return (
     <main>
-      <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" />
+      <Form onSave={handleSave}>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="age" id="age" />
+
+        <p>
+          <button>save</button>
+        </p>
+      </Form>
     </main>
   );
 }
