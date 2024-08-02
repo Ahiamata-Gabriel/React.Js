7dd6c823e729009117d78d0676f4bda8308bbbbd
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-02 00:17:06 +0000

    Container.tsx

---
+++ React-with-Typescript/1.dynamic-components/src/components/Container.tsx
@@ -0,0 +1,22 @@
+//*** POLYMORPHIC COMPONENTS ***
+import {
+  type ReactNode,
+  type ElementType,
+  type ComponentPropsWithoutRef,
+} from "react";
+
+type ContainerProps<T extends ElementType> = {
+  as?: T;
+  children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
+
+function Container<C extends ElementType>({
+  as,
+  children,
+  ...props
+}: ContainerProps<C>) {
+  const Component = as || "div";
+  return <Component {...props}>{children}</Component>;
+}
+
+export default Container;
