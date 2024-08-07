edd17ff01403ee00ea4d66a8ee927c484e3c34f3
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-07 02:30:40 +0000

    update

--- React-with-supabase/1.smoothies/src/components/SmoothiesCard.js
+++ React-with-supabase/1.smoothies/src/components/SmoothiesCard.js
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+
 const SmoothiesCard = ({ smoothie }) => {
-  const { title, method, rating } = smoothie;
+  const { title, method, rating, id } = smoothie;
   return (
     <div className="smoothie-card">
       <h3>{title}</h3>
       <p>{method}</p>
       <div className="rating">{rating}</div>
+      <div className="bottons">
+        <Link to={`/${id}`}>
+          <i className="material-icons">edit</i>
+        </Link>
+      </div>
     </div>
   );
 };
