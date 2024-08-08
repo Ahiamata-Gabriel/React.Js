d8e5d4a7a860e48d2a44d07446f5229a4df41ee9
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-08 00:51:20 +0000

    update

--- The Ultimate React Course/14-the-wild-oasis/src/App2.jsx
+++ The Ultimate React Course/14-the-wild-oasis/src/App2.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
