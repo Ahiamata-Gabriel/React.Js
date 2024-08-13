70d39483c6d635a4a2ab1be3ac60f09d2600108f
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-13 15:28:20 +0000

    main.jsx

--- The Ultimate React Course/14-the-wild-oasis/src/main.jsx
+++ The Ultimate React Course/14-the-wild-oasis/src/main.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.jsx";
-import App2 from "./App2.jsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-    {/* <App2 /> */}
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
