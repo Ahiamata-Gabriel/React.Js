1d9cda059bd3d647d51e8f93f22671a4410b11ee
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-14 01:23:37 +0000

    Cabins.jsx

--- The Ultimate React Course/14-the-wild-oasis/src/pages/Cabins.jsx
+++ The Ultimate React Course/14-the-wild-oasis/src/pages/Cabins.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -12,6 +17,11 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
