8db338a860dd959e382cb44012b4a50332782c94
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-08-25 16:03:37 +0000

    Finisheshed setting up online Environment

--- The Ultimate React Course/14-the-wild-oasis/src/pages/Dashboard.jsx
+++ The Ultimate React Course/14-the-wild-oasis/src/pages/Dashboard.jsx
@@ -5,7 +5,6 @@ function Dashboard() {
   return (
     <Row type="horizontal">
       <Heading as="h1">Dashboard</Heading>
-      <h1>Checking to see if everything is fine</h1>
       <p>TEST</p>
     </Row>
   );
