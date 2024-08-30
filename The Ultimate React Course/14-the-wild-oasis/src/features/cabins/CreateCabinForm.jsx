89661f3c544f92527943e2ba1f18ee7c06cc1881
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-08-30 05:20:21 +0000

    Form

--- The Ultimate React Course/14-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ The Ultimate React Course/14-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -46,7 +46,8 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-  const { register, handleSubmit, reset } = useForm();
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+  const { errors } = formState;
   const queryClient = useQueryClient();
 
   const { mutate, isLoading: isCreating } = useMutation({
@@ -54,33 +55,61 @@ function CreateCabinForm() {
     onSuccess: () => {
       toast.success("canin created successfully");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
-      reset()
+      reset();
     },
 
     onError: (err) => toast.error(err.message),
   });
-
 
   function onSubmit(data) {
     mutate(data);
     console.log(data);
   }
 
+  const onError = (errors) => {
+    console.log(errors);
+  };
+
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" {...register("name")} />
+        <Input
+          type="text"
+          id="name"
+          {...register("name", { required: "This field is required" })}
+        />
+        {errors?.name?.message && <Error>{errors.name.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" {...register("maxCapacitye")} />
+        <Input
+          type="number"
+          id="maxCapacity"
+          {...register("maxCapacitye", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Capacity should be atleast 1",
+            },
+          })}
+        />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" {...register("regularPrice")} />
+        <Input
+          type="number"
+          id="regularPrice"
+          {...register("regularPrice", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Capacity should be atleast 1",
+            },
+          })}
+        />
       </FormRow>
 
       <FormRow>
@@ -89,7 +118,12 @@ function CreateCabinForm() {
           type="number"
           id="discount"
           defaultValue={0}
-          {...register("discount")}
+          {...register("discount", {
+            required: "This field is required",
+            validate: (value) =>
+              value <= getValues().regularPrice ||
+              "Discount should be less than regular price",
+          })}
         />
       </FormRow>
 
@@ -99,7 +133,7 @@ function CreateCabinForm() {
           type="number"
           id="description"
           defaultValue=""
-          {...register("description")}
+          {...register("description", { required: "This field is required" })}
         />
       </FormRow>
 
