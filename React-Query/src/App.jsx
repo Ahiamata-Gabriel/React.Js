dde3542b544273e0c32ab971998c068bc231a1e9
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-09-11 04:20:54 +0000

    update

--- React-Query/src/App.jsx
+++ React-Query/src/App.jsx
@@ -1,25 +1,47 @@
 import "./App.css";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 function App() {
   const { data, error, isLoading } = useQuery({
     queryKey: ["todo"],
     queryFn: () =>
-      fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+      fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
         res.json()
       ),
   });
 
-  if (error) return <div>Failed to load data</div>;
+  const { mutate, isError, isPending, isSuccess } = useMutation({
+    mutationFn: (newPost) =>
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(newPost),
+      }).then((res) => res.json()),
+  });
 
-  if (isLoading) return <div>Data is loading</div>;
+  if (error || isError) return <div>Failed to load data</div>;
+
+  if (isLoading || isPending) return <div>Data is loading</div>;
 
   return (
     <div className="App">
+      <button
+        onClick={() =>
+          mutate({
+            userId: 44,
+            id: 144,
+            title: "BIG DAWG",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+          })
+        }
+      >
+        Add Post
+      </button>
+
       {data?.map((todo) => (
         <div key={todo.id}>
-          <h1>{todo.id}</h1>
-          <h1>{todo.title}</h1>
+          <h5>{todo.id}</h5>
+          <h3>{todo.title}</h3>
+          <p>{todo.body}</p>
         </div>
       ))}
     </div>
