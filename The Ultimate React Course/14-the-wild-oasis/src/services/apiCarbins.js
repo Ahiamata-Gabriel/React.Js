4aa5ee768dfdefc51311423623b05f56c73d7e00
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-09-14 06:09:34 +0000

    apiCarbins

--- The Ultimate React Course/14-the-wild-oasis/src/services/apiCarbins.js
+++ The Ultimate React Course/14-the-wild-oasis/src/services/apiCarbins.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCarbins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -14,14 +14,32 @@ export async function getCarbins() {
 
 // *** CREATING A NEW CARBIN  ***
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/carbin-images/${imageName}`;
+  //1. Create cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.log(error);
     throw new Error("Had problems creating carbins");
+  }
+  //2. Upload cabin
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  //3. Delete the cabin IF there was an  error uploading  the image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.log(storageError);
+    throw new Error("Cabin image not created, hence cabin wasnt created");
   }
 
   return data;
