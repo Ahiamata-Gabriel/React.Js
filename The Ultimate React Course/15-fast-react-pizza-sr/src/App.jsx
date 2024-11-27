9cacb548aa6376b4a5fc62f14ce3d96edbd243c4
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-11-27 19:11:47 +0000

    update

--- The Ultimate React Course/15-fast-react-pizza-sr/src/App.jsx
+++ The Ultimate React Course/15-fast-react-pizza-sr/src/App.jsx
@@ -1,4 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import Home from "./ui/Home";
+import Menu from "./ui/Menu";
+
+createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
 
 function App() {
   return <>Blood</>;
