00a6ab0021a24456fa960ff0dbdf4c2783abdfdc
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2024-12-19 15:15:50 +0000

    order

--- The Ultimate React Course/15-fast-react-pizza-sr/src/features/order/Order.jsx
+++ The Ultimate React Course/15-fast-react-pizza-sr/src/features/order/Order.jsx
@@ -8,54 +8,19 @@ import {
 } from "../../utilities/helpers";
 import { useLoaderData } from "react-router-dom";
 
-// const order = {
-//   id: "ABCDEF",
-//   customer: "Jonas",
-//   phone: "123456789",
-//   address: "Arroios, Lisbon , Portugal",
-//   priority: true,
-//   estimatedDelivery: "2027-04-25T10:00:00",
-//   cart: [
-//     {
-//       pizzaId: 7,
-//       name: "Napoli",
-//       quantity: 3,
-//       unitPrice: 16,
-//       totalPrice: 48,
-//     },
-//     {
-//       pizzaId: 5,
-//       name: "Diavola",
-//       quantity: 2,
-//       unitPrice: 16,
-//       totalPrice: 32,
-//     },
-//     {
-//       pizzaId: 3,
-//       name: "Romana",
-//       quantity: 1,
-//       unitPrice: 15,
-//       totalPrice: 15,
-//     },
-//   ],
-//   position: "-9.000,38.000",
-//   orderPrice: 95,
-//   priorityPrice: 19,
-// };
-
 function Order() {
   const order = useLoaderData();
   console.log(order);
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
-    // id,
+    id,
     status,
     priority,
     priorityPrice,
     orderPrice,
     estimatedDelivery,
-    // cart,
+    cart,
   } = order;
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
