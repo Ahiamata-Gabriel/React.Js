968d34525d92e991778d2b8bb8d8432f36f26dc2
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-01-16 15:55:12 +0000

    update

--- supabaseCrud/src/supabase-client.js
+++ supabaseCrud/src/supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://oqcezjvxgmxknublqljm.supabase.co";
-const supabaseKey = "";
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
